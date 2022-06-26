5867d05772fe1208dc2aa4367b12e88014697bcb
Aline <eualine2017@gmail.com>
2022-06-26 14:29:59 -0300

    feat: add mais um mock

--- backend/src/tests/mocks/taskMocks.js
+++ backend/src/tests/mocks/taskMocks.js
@@ -7,4 +7,26 @@ const taskMock = {
   atributo: 'importante',
 };
 
-module.exports = taskMock;
+const taskList = [
+  {
+    titulo: 'Tarefa 1',
+    descrição: 'Estudar',
+    data: '27-06-2022',
+    hora: '14:00',
+    duração: '08:00',
+    atributo: 'importante',
+  },
+  {
+    titulo: 'Tarefa 2',
+    descrição: 'Comer',
+    data: '27-06-2022',
+    hora: '17:00',
+    duração: '00:30',
+    atributo: 'importante',
+  },
+];
+
+module.exports = {
+  taskMock,
+  taskList,
+};
