f0c85878ad96ddd20be65af83b74fee1118ca11e
Aline <eualine2017@gmail.com>
2022-06-26 12:15:28 -0300

    feat: cria mock de task para testes

---
+++ backend/src/tests/mocks/taskMocks.js
@@ -0,0 +1,10 @@
+const taskMock = {
+  titulo: 'Tarefa 1',
+  descrição: 'Estudar',
+  data: '27-06-2022',
+  hora: '14:00',
+  duração: '08:00',
+  atributo: 'importante',
+};
+
+module.exports = taskMock;
