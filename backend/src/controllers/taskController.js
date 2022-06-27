533156cb1518cc5be5575b7aea695e34c78882cc
Aline <eualine2017@gmail.com>
2022-06-27 18:03:31 -0300

    feat(taskController): implementa create, update e delete em controller

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -9,6 +9,48 @@ const getAll = async (_req, res) => {
   }
 };
 
+const create = async (req, res) => {
+  const {
+    titulo, descrição, data, hora, duração, atributo,
+  } = req.body;
+  try {
+    const created = await taskService.create({
+      titulo, descrição, data, hora, duração, atributo,
+    });
+    res.status(201).json(created);
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  const {
+    titulo, descrição, data, hora, duração, atributo,
+  } = req.body;
+  try {
+    const updated = await taskService.update(id, {
+      titulo, descrição, data, hora, duração, atributo,
+    });
+    res.status(200).json(updated);
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await taskService.deleteTask(id);
+    res.status(200).json(deleted);
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+};
+
 module.exports = {
   getAll,
+  create,
+  update,
+  deleteTask,
 };
