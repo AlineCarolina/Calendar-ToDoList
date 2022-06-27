d5086ed5c8382e9ef2763058d7ecebfc74297c99
Aline <eualine2017@gmail.com>
2022-06-27 18:02:34 -0300

    feat(taskService): implementa create, update e delete em services

--- backend/src/services/taskService.js
+++ backend/src/services/taskService.js
@@ -5,6 +5,33 @@ const getAll = async () => {
   return all;
 };
 
+const create = async ({
+  titulo, descrição, data, hora, duração, atributo,
+}) => {
+  const newTask = new Task({
+    titulo, descrição, data, hora, duração, atributo,
+  });
+  const created = await newTask.save();
+  return created;
+};
+
+const update = async (id, {
+  titulo, descrição, data, hora, duração, atributo,
+}) => {
+  const updated = await Task.findByIdAndUpdate(id, {
+    titulo, descrição, data, hora, duração, atributo,
+  });
+  return updated;
+};
+
+const deleteTask = async (id) => {
+  const deleted = await Task.findByIdAndDelete(id);
+  return deleted;
+};
+
 module.exports = {
   getAll,
+  create,
+  update,
+  deleteTask,
 };
